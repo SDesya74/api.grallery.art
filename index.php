30a29675bacea865cd7701aff618ff96b6cb055a
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-01-09 00:19:43 +0300

    Add CORS

--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 error_reporting(E_ALL);
 
 require_once "vendor/autoload.php";
+
+// region CORS
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS");
+    header("Access-Control-Allow-Headers: token, Content-Type");
+    header("Access-Control-Max-Age: 1728000");
+    header("Content-Length: 0");
+    header("Content-Type: text/plain");
+    die();
+}
+
+header("Access-Control-Allow-Origin: *");
+// endregion
+
+// region Connect to database with RedBean
+
 require_once "lib/rb.php";
 require_once "variables/db.php";
 
@@ -10,10 +27,18 @@ if (isset($db_host) && isset($db_user) && isset($db_pass)) {
     R::setup($db_host, $db_user, $db_pass);
 }
 
+// endregion
+
+// region Get all routes
+
 $collector = new Phroute\RouteCollector();
 foreach (glob("endpoints/*.php") as $endpoint) {
     include_once $endpoint;
 }
+
+// endregion
+
+// region Working with request
 
 function send_response($response) {
     $code = $response[0];
@@ -44,4 +69,6 @@ try {
         response(400, [ "message" => "Bad route" ])
     );
 }
+
+// endregion
 
