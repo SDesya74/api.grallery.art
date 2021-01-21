1816ce39e18da48f34c696aa0547218c2643ff8b
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-01-21 09:20:04 +0300

    Add pagination to /user/:username/posts endpoint

--- endpoints/user[username]posts.php
+++ endpoints/user[username]posts.php
@@ -10,18 +10,10 @@ $collector->get(
         $user_bean = R::findOne("user", "username LIKE :username", [ ":username" => $username ]);
         if ($user_bean === null) return error("User not found");
 
-        $page = Request::page();
+        [ "limit" => $limit, "offset" => $offset ] = Request::page();
 
         $total = $user_bean->countOwn("post");
-        $post_beans = R::findAll(
-            "post",
-            "WHERE user_id = :user_id LIMIT :offset, :limit",
-            [
-                ":user_id" => $user_bean->id,
-                ":offset" => $page->offset,
-                ":limit" => $page->limit
-            ]
-        );
+        $post_beans = R::findAll("post", "WHERE user_id = ? LIMIT ?, ?", [ $user_bean->id, $offset, $limit ]);
 
         $posts = [];
         foreach ($post_beans as $post) {
@@ -32,27 +24,6 @@ $collector->get(
             $posts[] = $post;
         }
 
-        $links = [];
-        if ($page->offset + $page->limit < $total) $links["next"] = urldecode(
-            http_build_url(
-                $_SERVER["REQUEST_URI"],
-                [ "query" => http_build_query(
-                    [ "page" => [ "offset" => $page->offset + $page->limit ] ]
-                ) ],
-                HTTP_URL_STRIP_AUTH | HTTP_URL_JOIN_PATH | HTTP_URL_JOIN_QUERY | HTTP_URL_STRIP_FRAGMENT
-            )
-        );
-
-        if ($page->offset >= $page->limit) $links["prev"] = urldecode(
-            http_build_url(
-                $_SERVER["REQUEST_URI"],
-                [ "query" => http_build_query(
-                    [ "page" => [ "offset" => $page->offset - $page->limit ] ]
-                ) ],
-                HTTP_URL_STRIP_AUTH | HTTP_URL_JOIN_PATH | HTTP_URL_JOIN_QUERY | HTTP_URL_STRIP_FRAGMENT
-            )
-        );
-
-        return ok($posts, [ "page" => $page, "links" => $links ]);
+        return ok($posts, pagination($limit, $offset, $total));
     }
 );
