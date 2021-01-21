a55deef17691f6008156975b1d508710b792c3d2
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-01-21 09:18:29 +0300

    Add Authorization header to CORS and fix endpoints glob pattern

--- index.php
+++ index.php
@@ -1,45 +1,41 @@
 <?php
-
 error_reporting(E_ALL);
 
 require_once "vendor/autoload.php";
 
+require_once "variables/token.php";
+require_once "variables/captcha.php";
+
 // region CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS");
-    header("Access-Control-Allow-Headers: token, Content-Type");
+    header("Access-Control-Allow-Headers: Authorization, Content-Type");
     header("Access-Control-Max-Age: 1728000");
     header("Content-Length: 0");
-    header("Content-Type: text/plain");
+    header("Content-Type: application/json");
     die();
 }
-
-header("Access-Control-Allow-Origin: *");
 // endregion
 
 // region Connect to database with RedBean
-
 require_once "lib/rb.php";
 require_once "variables/db.php";
 
 if (isset($db_host) && isset($db_user) && isset($db_pass)) {
     R::setup($db_host, $db_user, $db_pass);
 }
-
 // endregion
 
 // region Get all routes
-
 $collector = new Phroute\RouteCollector();
-foreach (glob("endpoints/*.php") as $endpoint) {
+foreach (glob("endpoints/**/*.php") as $endpoint) {
     include_once $endpoint;
 }
-
 // endregion
 
 // region Working with request
-
 function send_response($response) {
     $code = $response[0];
     $result = $response[1];
@@ -69,6 +65,4 @@ try {
         response(400, [ "message" => "Bad route" ])
     );
 }
-
 // endregion
-
