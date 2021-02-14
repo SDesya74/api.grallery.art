758539eca1190258dc876db9418c9c2a35bf731f
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-02-14 04:47:04 +0300

    Use FieldFilter in endpoints

--- endpoints/me.php
+++ endpoints/me.php
@@ -13,11 +13,7 @@ $collector->get(
         $user_bean = R::findOne("user", "id = ?", [ $payload->id ]);
         if ($user_bean == null) return error("User not found");
 
-        $user_bean->id = (int) $user_bean->id;
-        $user_bean->created = (int) $user_bean->created;
-        $user_bean->last_enter = (int) $user_bean->last_enter;
-        unset($user_bean->password_hash);
-
-        return ok($user_bean);
+        $fields = Request::fields("user");
+        return ok($user_bean->getFields($fields));
     }
 );
--- endpoints/user[username].php
+++ endpoints/user[username].php
@@ -6,23 +6,11 @@ if (!isset($collector)) return;
 $collector->get(
     "/user/{username:[a-zA-Z0-9-_]{2,30}}",
     function($username) {
-        $fields = Request::fields("user");
 
         $user_bean = R::findOne("user", "username LIKE :username", [ ":username" => $username ]);
         if ($user_bean === null) return error("User not found");
 
-        unset($user_bean->password_hash);
-
-        if (!empty($fields)) {
-            $result = array_filter(
-                $user_bean->jsonSerialize(),
-                function($key) use ($fields) {
-                    return in_array($key, $fields);
-                },
-                ARRAY_FILTER_USE_KEY
-            );
-        }else $result = $user_bean;
-
-        return ok($result, hateoas("posts","/user/$username/posts"));
+        $fields = Request::fields("user");
+        return ok($user_bean->getFields($fields), hateoas("posts", "/user/$username/posts"));
     }
 );
