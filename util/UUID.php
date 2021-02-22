8f1f15c117408f8b4471d54c2ad3fa29870023b5
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-02-22 16:26:18 +0300

    Create UUID_LENGTH constant and regex to test it

--- util/UUID.php
+++ util/UUID.php
@@ -4,36 +4,52 @@ use RedBeanPHP\Adapter;
 use RedBeanPHP\QueryWriter;
 use RedBeanPHP\QueryWriter\MySQL;
 
+$UUID_LENGTH = 11;
+
 class UUIDWriterMySQL extends MySQL implements QueryWriter {
     const C_DATATYPE_SPECIAL_UUID = 97;
     protected $defaultValue = "@uuid";
 
     public function __construct(Adapter $adapter) {
+        global $UUID_LENGTH;
         parent::__construct($adapter);
         $this->addDataType(
             self::C_DATATYPE_SPECIAL_UUID,
-            "char(36)");
+            "char($UUID_LENGTH)");
     }
 
     public function createTable($table) {
+        global $UUID_LENGTH;
         $table = $this->esc($table);
         $sql = "CREATE TABLE {$table} (
-                id char(36) NOT NULL,
-                PRIMARY KEY ( id ))
+                id char($UUID_LENGTH) NOT NULL,
+                PRIMARY KEY (id))
                 ENGINE = InnoDB DEFAULT
-                CHARSET=utf8mb4
-                COLLATE=utf8mb4_unicode_ci";
+                CHARSET = utf8mb4
+                COLLATE = utf8mb4_unicode_ci";
         $this->adapter->exec($sql);
     }
 
     public function updateRecord($table, $updateValues, $id = null) {
+        global $UUID_LENGTH;
+
         $flagNeedsReturnID = (!$id);
-        // trim(trailing '=' from to_base64(replace(uuid(), '-', '')))
-        // replace(uuid(), '-', '')
-        if ($flagNeedsReturnID) R::exec("SET @uuid = uuid()");
+
+        do $uid = $this->generateUid($UUID_LENGTH);
+        while (R::count($table, "id = ?", [ $uid ]) > 0);
+
+        if ($flagNeedsReturnID) R::exec("SET @uuid = ?;", [ $uid ]);
         $id = parent::updateRecord($table, $updateValues, $id);
         if ($flagNeedsReturnID) $id = R::getCell("SELECT @uuid");
         return $id;
+    }
+
+    private function generateUid($length) {
+        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-'; // [A-Za-z-_]{11}
+        $chars_length = strlen($chars);
+        $uid = '';
+        while ($length-- > 0) $uid .= $chars[random_int(0, $chars_length)];
+        return $uid;
     }
 
     public function getTypeForID() {
