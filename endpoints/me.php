62fe4e3364dd453f0ec0a9a9da901dd508a3dccb
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-05-23 14:00:09 +0300

    Remove fields in request and add /auctions endpoint

--- endpoints/me.php
+++ endpoints/me.php
@@ -5,14 +5,24 @@ $collector->get(
     "/me",
     function () {
         $token = AccessToken::get();
-
         $user_bean = Model_User::byID($token->getUserID());
         if ($user_bean == null) return error("User not found");
 
         $username = $user_bean->username;
 
-        $fields = Request::fields("user");
-        return ok($user_bean->getFields($fields), hateoas("posts", "user/$username/posts"));
+        return ok($user_bean, hateoas("posts", "user/$username/posts"));
+    },
+    [ "before" => "auth" ]
+);
+
+$collector->get(
+    "/auctions",
+    function () {
+        $userID = AccessToken::get()->getUserID();
+        $user = Model_User::byID($userID);
+        if ($user == null) return not_found("User not found");
+
+        return ok($user->ownAuctionList);
     },
     [ "before" => "auth" ]
 );
