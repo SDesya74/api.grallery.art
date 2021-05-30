99f9801acf1f41f8f1d8be9f76febadbb786bc1e
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-05-30 14:09:23 +0300

    Add max session count

--- models/Model_Session.php
+++ models/Model_Session.php
@@ -1,7 +1,6 @@
 <?php
 
 use JetBrains\PhpStorm\ArrayShape;
-use RedBeanPHP\OODBBean;
 
 class Model_Session extends RedBean_SimpleModel {
     #[ArrayShape([ "access" => "\ArrayObject", "refresh" => "\ArrayObject", "server_time" => "integer" ])]
@@ -19,6 +18,16 @@ class Model_Session extends RedBean_SimpleModel {
         // save refresh token to database
         $user->ownSessionList[] = $session_bean;
 
-        return [ "access" => $access, "refresh" => $refresh ];
+        // remove old sessions
+        $count = count($user->ownSessionList);
+        $max = 10;
+        if ($count >= $max) {
+            $sessions = array_values($user->ownSessionList);
+            $sessions_to_hunt = array_slice($sessions, 0, $count - $max);
+            $ids = array_column($sessions_to_hunt, "id");
+            R::hunt("session", "id IN (" . R::genSlots($ids) . ")", $ids);
+        }
+
+        return [ "access" => $access, "refresh" => $refresh, "server_time" => time() ];
     }
 }
